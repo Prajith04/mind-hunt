9cac6a7d459c995f6254f5721d1a6a4f74678f98
Prajith21 <prajith_22aib21@kgkite.ac.in>
2024-08-15 17:05:57 +0530

    removed alert

--- src/careerQuestions.jsx
+++ src/careerQuestions.jsx
@@ -832,7 +832,6 @@ function Quiz({setScores}) {
     } else {
       // Quiz completed, display results
       const topScoringTopics = Object.keys(topicScores).sort((a, b) => topicScores[b] - topicScores[a]).slice(0, 3);
-      alert('Quiz completed! Your scores:\n' + JSON.stringify(topicScores, null, 2));
       setScores(topicScores)
       const careerMap = {
         codingTheory: ['Software Engineer', 'Developer', 'Programmer'],
@@ -844,7 +843,6 @@ function Quiz({setScores}) {
         gameDevelopment: ['Game Developer', 'Game Designer', '3D Artist'],
       };
       const recommendedCareers = topScoringTopics.flatMap((topic) => careerMap[topic]);
-      alert('Based on your scores, we recommend the following careers:\n' + JSON.stringify(recommendedCareers));
       navigate('/careerresult', { 
         state: { 
           scores: topicScores, 
